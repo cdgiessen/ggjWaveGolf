ad74b213b57e4f0172abcc8b9d265d651c32e96e
cdgiessen <cdgiessen@gmail.com>
2017-01-21 20:21:04 -0600

    Eyetracking???

---
+++ Assets/Scripts/SpinOnGaze.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.EyeTracking;
+
+[RequireComponent(typeof(GazeAware))]
+public class SpinOnGaze : MonoBehaviour
+{
+    private GazeAware _gazeAware;
+
+    void Start()
+    {
+        _gazeAware = GetComponent<GazeAware>();
+    }
+
+    void Update()
+    {
+
+        if (_gazeAware.HasGazeFocus)
+        {
+            transform.Rotate(Vector3.forward);
+            Debug.Log("Lost Focus");
+        }
+        GazePoint gazePoint = EyeTracking.GetGazePoint();
+        if (gazePoint.IsValid)
+        {
+            print("Gaze point on Screen (X,Y): " + gazePoint.Screen.x + ", " + gazePoint.Screen.y);
+        }
+    }
+}
